46eb0a926dd1bf62cc3f4599c0194f650dcdc32d
Ma Hawa Deploy <deploy@mahawa.app>
2026-05-25 20:48:01 +0000

    Restructure: move from src/ to root app/ for Vercel compatibility

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const createNextIntlPlugin = require("next-intl/plugin");
 
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig = {
   reactStrictMode: true,
